15e101dd338c94d75a2f51e438ca5e6a06b07f24
cameron <me@you.com>
2021-11-11 09:36:45 +0000

    this is a prompt-sync update

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+var prompt = require('prompt-sync')();
+//
+// get input from the user.
+//
+var n = prompt('Tell me what you wanna say: ');
+console.log('You said ', n)
+
 console.log('hello world');
-console.log('im pushing a new iteration');
-console.log('this is update number 2');
